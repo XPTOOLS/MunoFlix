3907826072352af30d4c679c9ef9bba6d3b1c8ad
XP TOOLS <freenethubbusiness@gmail.com>
2025-11-24 11:46:30 +0200

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/partials/header/Header";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { UserInfoProvider } from "@/context/UserInfoContext";
+import VisitTracker from "@/components/analytics/VisitTracker";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default async function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <UserInfoProvider>
+          <Header />
+          <VisitTracker />
+          {children}
+        </UserInfoProvider>
+        <ToastContainer draggable theme="dark" />
+      </body>
+    </html>
+  );
+}
