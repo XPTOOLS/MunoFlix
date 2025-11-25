18b212f76865042fab62bfe0c4049587a649a0f6
XP TOOLS <freenethubbusiness@gmail.com>
2025-11-25 11:29:39 +0200

    Delete components directory

--- components/loadings/loadingVideo/loadingVideo.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./loadingVideos.module.css"
-
-const LoadingVideo = () => {
-  return (
-    <div className={styles.loading}><div className={styles.i}><div></div><div></div></div></div>
-  )
-}
-
-export default LoadingVideo
